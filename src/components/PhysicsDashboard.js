3c551bbc7e39355ff38d94c06eb8002c404ba2d2
samadhi1000 <samadhi1000@gmail.com>
2026-06-24 00:17:19 +0530

    style: adjust Einstein vertical background alignment to show face on desktop

--- src/components/PhysicsDashboard.js
+++ src/components/PhysicsDashboard.js
@@ -44,7 +44,7 @@ const heroSlides = [
     image: '/images/hero_einstein.png',
     si: 'ඇල්බට් අයින්ස්ටයින්',
     en: 'Albert Einstein',
-    position: 'center',
+    position: 'center 10%',
     size: 'cover',
     mirror: false
   },
