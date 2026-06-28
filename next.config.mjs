0f016a97b0094d918deb9fbf41cc816e71a78435
samadhi1000 <samadhi1000@gmail.com>
2026-06-28 22:46:40 +0530

    Add redirect from root to /si for Google verification

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/si',
+        permanent: true,
+      },
+    ];
+  },
 };
-
 
 export default nextConfig;
